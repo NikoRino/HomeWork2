d1912d469aff9cd7b5b73b6cf0b58bef3a9e80b5
NikoRino <niko89rino@gmail.com>
2023-07-12 19:41:00 +0700

    Добавил две задачи

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите трехзначное число:");
+
+int Value = Convert.ToInt32(Console.ReadLine());
+int Length = Value.ToString().Length;
+if (Length == 3)
+{
+    int result = (Value / 10) % 10;
+    Console.WriteLine($"Вторая цифра {result}");
+}
+else
+{
+    Console.WriteLine("Не трехзначное число");
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число:");
+
+int Value = Convert.ToInt32(Console.ReadLine());
+int Length = Value.ToString().Length;
+if (Length > 2)
+{
+    int result = (Value / 100) % 10;
+    Console.WriteLine($"Третья цифра {result}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
